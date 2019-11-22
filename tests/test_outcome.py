e2e949b4a4b2c4ff2c30b9502b5834d4a6dc4e3c
superelectron <matmccann@gmail.com>
2019-11-21 20:21:22 -0800

    update tests

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -10,31 +10,37 @@ class TestOutcome(unittest.TestCase):
         self.outcome3 = Outcome("first-12", 2)
         self.outcome4 = Outcome("split-bet", 19)
 
-    def test_equal(self):
+    def test_setup(self):
+        """ verifying setUp variables for test suite """
         self.assertEqual(self.outcome1, self.outcome2)
+        self.assertNotEqual(self.outcome1, self.outcome3)
+        self.assertEqual(self.outcome1.getName(), self.outcome2.getName())
 
     def test___eq__(self):
+        """ testing __eq__ """
         self.assertTrue(self.outcome1.__eq__(self.outcome2))
         self.assertFalse(self.outcome1.__eq__(self.outcome3))
 
     def test___ne__(self):
+        """ testing __ne__ """
         self.assertTrue(self.outcome1.__ne__(self.outcome3))
         self.assertFalse(self.outcome1.__ne__(self.outcome1))
 
     def test___str__(self):
+        """ testing __str__ """
         self.assertEqual(str(self.outcome1), "red (17:1)")
         self.assertEqual(self.outcome1.__str__(), "red (17:1)")
 
     def test___repr__(self):
+        """ testing __repr__"""
         self.assertEqual(self.outcome1.__repr__(), "red (17:1)")
 
-    def test_unequal(self):
-        self.assertNotEqual(self.outcome1, self.outcome3)
-
-    def test_win_amount(self):
+    def test_winAmount(self):
+        """ testing  winAmount"""
         self.assertEqual(self.outcome1.winAmount(10), 170)
 
     def test_hash(self):
+        """ testing __hash__ """
         self.assertEqual(hash(self.outcome1), hash(self.outcome2))
         self.assertNotEqual(hash(self.outcome1), hash(self.outcome3))
 
